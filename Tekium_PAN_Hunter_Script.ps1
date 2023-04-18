64b8b03e9ff32a84de63df0b991b685d75a42111
rerick38 <54648182+rerick38@users.noreply.github.com>
2023-04-18 15:25:52 -0600

    Changes version 1.1

--- Tekium_PAN_Hunter_Script.ps1
+++ Tekium_PAN_Hunter_Script.ps1
@@ -1,12 +1,9 @@
-﻿cls
+﻿Param([string] $path_search, [string[]] $filters)
+cls
 $dir_actual = (pwd).ToString()
 $hostname = hostname
-$ip = Test-Connection -ComputerName(hostname) -Count 1 | Select-Object ipv4address
-$ip_address = $ip.IPV4Address.ToString()
 $date_actual = Get-Date -Format "yyyy_MM_dd"
 $log = $dir_actual + "\tekium_pan_hunter_$date_actual.log"
-$path_search = 'C:\'
-$filters = '*.txt', '*.csv', '*.doc', '*.docx', '*.xls', '*.xlsx', '*.log'
 $regex_amex = '([^0-9-]|^)(3(4[0-9]{2}|7[0-9]{2})( |-|)[0-9]{6}( |-|)[0-9]{5})([^0-9-]|$)'
 $regex_visa = '([^0-9-]|^)(4[0-9]{3}( |-|)([0-9]{4})( |-|)([0-9]{4})( |-|)([0-9]{4}))([^0-9-]|$)'
 $regex_master = '([^0-9-]|^)(5[0-9]{3}( |-|)([0-9]{4})( |-|)([0-9]{4})( |-|)([0-9]{4}))([^0-9-]|$)'
@@ -14,27 +11,50 @@ Write-Host "--------------------------------------------------------------------
 write-host "Copyright©Tekium 2023. All rights reserved." -ForegroundColor green
 write-host "Author: Erick Roberto Rodriguez Rodriguez" -ForegroundColor green
 write-host "Email: erodriguez@tekium.mx, erickrr.tbd93@gmail.com" -ForegroundColor green
-write-host "Tekium PAN Hunter Script v1.0" -ForegroundColor green
+write-host "Tekium PAN Hunter Script v1.1" -ForegroundColor green
 Write-Host "-------------------------------------------------------------------------------------" -ForegroundColor Yellow
 Write-Host "Hostname: $hostname"
 Write-Host ""
-Write-Host "IP Address: $ip_address"
+Write-Host "Path: $path_search"
 Write-Host ""
-$pans = Get-ChildItem -Path $path_search -Recurse -include $filters | Select-String -Pattern $regex_amex, $regex_visa, $regex_master -AllMatches | Select-Object Filename, Line
-if ( $pans )
-{
-    Write-Host "PAN's found" -ForegroundColor green
-    "-------------------------------------------------------------------------------------" | Out-File -FilePath $log -Append
-    "Copyright©Tekium 2023. All rights reserved." | Out-File -FilePath $log -Append 
-    "Author: Erick Roberto Rodriguez Rodriguez" | Out-File -FilePath $log -Append
-    "Email: erodriguez@tekium.mx, erickrr.tbd93@gmail.com" | Out-File -FilePath $log -Append
-    "Tekium PAN Hunter Script v1.0" | Out-File -FilePath $log -Append
-    "-------------------------------------------------------------------------------------" | Out-File -FilePath $log -Append
-    "Hostname: $hostname" | Out-File -FilePath $log -Append
-    "IP Address: $ip_address" | Out-File -FilePath $log -Append
-    "Path: $path_search" | Out-File -FilePath $log -Append
-    $pans | Out-File -FilePath $log -Append
+Write-Host "Filters: $filters"
+Write-Host ""
+Write-Host "Start the file search with the filters (it takes a few minutes)"
+Write-Host ""
+$files_count = (Get-ChildItem -Path $path_search -Recurse -include $filters | Measure-Object).Count -as [int]
+if ($files_count -gt 0) {
+    write-host "$files_count files found" -ForegroundColor green
+    Write-Host ""
+    Write-Host "Start the search for PAN's in the found files (it takes a few minutes)"
+    Write-Host ""
+    $names_files = Get-ChildItem -Path $path_search -Recurse -include $filters
+    $names_files | ForEach-Object -Begin{
+        $i = 0
+        "-------------------------------------------------------------------------------------" | Out-File -FilePath $log -Append
+        "Copyright©Tekium 2023. All rights reserved." | Out-File -FilePath $log -Append 
+        "Author: Erick Roberto Rodriguez Rodriguez" | Out-File -FilePath $log -Append
+        "Email: erodriguez@tekium.mx, erickrr.tbd93@gmail.com" | Out-File -FilePath $log -Append
+        "Tekium PAN Hunter Script v1.1" | Out-File -FilePath $log -Append
+        "-------------------------------------------------------------------------------------" | Out-File -FilePath $log -Append
+        "Hostname: $hostname" | Out-File -FilePath $log -Append
+        "Path: $path_search" | Out-File -FilePath $log -Append
+        "Filters: $filters" | Out-File -FilePath $log -Append
+    } -Process{
+        $pans = Select-String -Path $_.FullName -Pattern $regex_amex, $regex_visa, $regex_master -AllMatches -ErrorAction SilentlyContinue | Select-Object Filename, Line
+        if ( $pans ){
+            $pans | Out-File -FilePath $log -Append
+            Write-Host "PAN's found in: $_.FullName" -ForegroundColor Green
+            Write-Host ""
+        }
+        $i = $i+1
+        $Completed = ($i/$names_files.count) * 100
+        Write-Progress -Activity "Searching PAN's in: $_.FullName" -Status "Progress:" -PercentComplete $Completed
+    } -End{
+        Write-Host "The search for PAN's ended" -ForegroundColor Green
+        Write-Host ""
+    }
 }
 else{
-    Write-Host "No PAN's found" -ForegroundColor Red
+    Write-Host "No files found" -ForegroundColor Red
+    Write-Host ""
 }
