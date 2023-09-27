89cb96dde8d551e711b08048e7bfb6d01aee3a13
Erick Rodriguez <erickrr.tbd93@gmail.com>
2023-09-27 13:02:33 -0600

    Change regex for PCRE expressions

--- Tekium_PAN_Hunter_Script.sh
+++ Tekium_PAN_Hunter_Script.sh
@@ -15,34 +15,37 @@ clear
 path_search=$1
 current_date=`date +"%d-%m-%Y"`
 log="tekium_pan_hunter_$current_date.log"
-regex_amex='([^0-9-]|^)(3(4[0-9]{2}|7[0-9]{2})( |-|)[0-9]{6}( |-|)[0-9]{5})([^0-9-]|$)'
-regex_visa='([^0-9-]|^)(4[0-9]{3}( |-|)([0-9]{4})( |-|)([0-9]{4})( |-|)([0-9]{4}))([^0-9-]|$)'
-regex_master='([^0-9-]|^)(5[0-9]{3}( |-|)([0-9]{4})( |-|)([0-9]{4})( |-|)([0-9]{4}))([^0-9-]|$)'
+regex_amex_without_spaces='3[47]\d{14}'
+regex_visa_without_spaces='4\d{15}'
+regex_master_without_spaces='5[1-5]\d{14}'
+regex_amex_characters='3[47]\d{2}[-\s]\d{4}[-\s]\d{4}[-\s]\d{4}'
+regex_visa_characters='4\d{3}[-\s]\d{4}[-\s]\d{4}[-\s]\d{4}'
+regex_master_characters='5[1-5]\d{2}[-\s]\d{4}[-\s]\d{4}[-\s]\d{4}'
 echo -e "\033[33m-------------------------------------------------------------------------------\033[0m" | tee -a $log
 echo -e "\033[32mCopyright©Tekium 2023. All rights reserved.\033[0m" | tee -a $log
 echo -e "\033[32mAuthor: Erick Roberto Rodriguez Rodriguez\033[0m" | tee -a $log
 echo -e "\033[32mEmail: erodriguez@tekium.mx, erickrr.tbd93@gmail.com\033[0m" | tee -a $log
 echo -e "\033[32mGitHub: https://github.com/erickrr-bd/Tekium-PAN-Hunter-Script\033[0m" | tee -a $log
-echo -e "\033[32mTekium PAN Hunter Script for Linux v1.1.2 - June 2023\033[0m" | tee -a $log
+echo -e "\033[32mTekium PAN Hunter Script for Linux v1.1.3 - September 2023\033[0m" | tee -a $log
 echo -e "\033[33m-------------------------------------------------------------------------------\033[0m" | tee -a $log
 echo -e "\nHostname: $HOSTNAME\n" | tee -a $log
 echo -e "Path: $path_search\n" | tee -a $log
 echo -e "Searching for files with the filters set (this may take several minutes):\n"
-files_count=$(find $path_search -regextype posix-egrep -iregex '.*\.(txt|csv|docx|xlsx|log|xls|doc)$' -type f 2>/dev/null | wc -l) 
+files_count=$(find $path_search -regextype posix-egrep -iregex '.*\.(txt|csv|docx|xlsx|log|xls|doc|xml)$' -type f 2>/dev/null | wc -l) 
 if [[ $files_count -gt 0 ]];then
 	i=0
 	files_no_pans=0
 	echo -e "\033[32m$files_count files found\033[0m\n" | tee -a $log
 	echo -e "Searching for possible PANs in the found files (this may take several minutes):"
-	names_files=$(find $path_search -regextype posix-egrep -iregex '.*\.(txt|csv|docx|xlsx|log|xls|doc)$' -type f 2>/dev/null) 
+	names_files=$(find $path_search -regextype posix-egrep -iregex '.*\.(txt|csv|docx|xlsx|log|xls|doc|xml)$' -type f 2>/dev/null) 
 	for file in $names_files
 	do
 		if [ -f "$file" ]; then
 			echo -e "\nSearch in: $file"
-    		result=$(egrep -l "$regex_amex|$regex_visa|$regex_master" $file 2>/dev/null)
-    		if [ $result ];then
-    			echo -e "\n\033[32mPossible PANs found in: $result\033[0m\n" | tee -a $log
-    			lines=$(egrep -o "$regex_amex|$regex_visa|$regex_master" $file 2>/dev/null | head -n5 | awk '{
+    		result=$(grep -Po "$regex_amex_without_spaces|$regex_visa_without_spaces|$regex_master_without_spaces|$regex_amex_characters|$regex_visa_characters|$regex_master_characters" $file 2>/dev/null)
+    		if [[ $result ]];then
+    			echo -e "\n\033[32mPossible PANs found in: $file\033[0m\n" | tee -a $log
+    			lines=$(grep -Po "$regex_amex_without_spaces|$regex_visa_without_spaces|$regex_master_without_spaces|$regex_amex_characters|$regex_visa_characters|$regex_master_characters" $file 2>/dev/null  | awk '{
                		begin=substr($0, 2, 12)
                		rest=substr($0, 14)
                		gsub(/./, "x", begin)
